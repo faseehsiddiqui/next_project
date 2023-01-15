8754e1b9258882a47cddbdf3b1b894293fd9d504
faseehsiddiqui <faseehsiddiqui564@gmail.com>
2023-01-15 16:02:33 +0500

    second change

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App my next app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
